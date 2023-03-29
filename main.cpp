eab86ec1fdd4c6052328d448cf47ae4f0213c851
Vasile Holonec <Vasile.Holonec@analog.com>
2023-03-29 15:32:46 +0300

    Read just from one channel
    Signed-off-by: Vasile Holonec <Vasile.Holonec@analog.com>

--- main.cpp
+++ main.cpp
@@ -24,14 +24,14 @@ int main()
 
         /* Getting nwtowk iio device context */
         std::string uri = "ip:127.0.0.1";
-        m_network_context = iio_create_context_from_uri(uri.c_str());
-        m_object_context =  iio_context_clone(m_network_context);
+       // m_network_context = iio_create_context_from_uri(uri.c_str());
+       // m_object_context =  iio_context_clone(m_network_context);
 
-        //ctx= iio_create_local_context();
+        ctx= iio_create_local_context();
 
-        m_dev = iio_context_find_device(m_object_context, "adis16505");
+        m_dev = iio_context_find_device(ctx, "adis16505");
         dev_name = iio_device_get_name(m_dev);
-        m_channel_accel_x = iio_device_find_channel(m_dev, "accel_x", false);
+        //m_channel_accel_x = iio_device_find_channel(m_dev, "accel_x", false);
 
         int channel_count = 0;
         printf("* Initializing IIO streaming channels:\n");
@@ -54,25 +54,27 @@ int main()
                 channels[i] = chn;
         }
 
-        for (int i = 0; i < channel_count; ++i)
-        {
-                iio_channel_enable(channels[i]);
-                if (iio_channel_is_enabled(channels[i]) == true)
-                {
-                    printf("channel_enabled %d \n", i);
-                }
-        }
+        iio_channel_enable(channels[0]);
+//        for (int i = 0; i < channel_count; ++i)
+//        {
+//                iio_channel_enable(channels[i]);
+//                if (iio_channel_is_enabled(channels[i]) == true)
+//                {
+//                    printf("channel_enabled %d \n", i);
+//                }
+//        }
 
 
-        iio_channel_enable(m_channel_accel_x);
-        if (iio_channel_is_enabled(m_channel_accel_x)  ==true)
+       // iio_channel_enable(m_channel_accel_x);
+        //if (iio_channel_is_enabled(m_channel_accel_x)  ==true)
         {
-            printf(" channel_enabled accel x \n");
+        //    printf(" channel_enabled accel x \n");
         }
 
 
         uint32_t sample_size = 4;
-        uint32_t buffer_length = 1;
+        uint32_t buffer_length = 8;
+         uint32_t buffer_mask = 1;
 
         struct iio_buffer *device_buffer =
                 iio_device_create_buffer(m_dev, buffer_length, false);
@@ -86,6 +88,7 @@ int main()
         int count =0;
 
         while (true) {
+                //device_buffer->mask = buffer_mask;
                 ssize_t nbytes_rx = iio_buffer_refill(device_buffer);
                 count++;
                 if (nbytes_rx <= 0) {
